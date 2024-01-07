d319dfc0c62f2e834eebc99eaec02eccdbb9399e
Neda bashiri <neda.bashiri.dev@gmail.com>
2024-01-07 11:57:58 +0330

    Update log4j-example class and use of log4j.

--- src/Log4jExample.java
+++ src/Log4jExample.java
@@ -1,5 +1,19 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Log4jExample {
+
+    private static final Logger LOGGER = LogManager.getLogger(Log4jExample.class);
+
     public static void main(String[] args) {
+
+        System.out.println("Log4j");
+        LOGGER.trace("This is a TRACE message.");
+        LOGGER.debug("This is a DEBUG message.");
+        LOGGER.info("This is an INFO message.");
+        LOGGER.warn("This is a WARN message.");
+        LOGGER.error("This is an ERROR message.");
+        LOGGER.fatal("This is a FATAL message.");
 
     }
 }
